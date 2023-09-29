8b02b14c37f4f92cfcdea7328ed186ee91380595
NandiniGajula <113129037+NandiniGajula@users.noreply.github.com>
2023-09-29 10:41:17 +0530

    Add files via upload
    MDI Window Forms

---
+++ Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Authent
+{
+    public partial class Login : Form
+    {
+        String username, password;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            password = textBox2.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(username=="admin"&& password=="admin")
+            {
+                MessageBox.Show("login successfully");
+                form4 form4 = new form4();
+                form4.Show();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect username and password");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            username = textBox1.Text;
+        }
+    }
+}
---
+++ Register.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Authent
+{
+    public partial class Register : Form
+    {
+        String username, password, confirmpassword;
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            password = textBox2.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (username.Length < 3 || username.Length > 16)
+            {
+                MessageBox.Show("username should have atleast 3 to 16  characters");
+                return;
+            }
+             else if (password != confirmpassword)
+            {
+                MessageBox.Show("paasword and confirmpassword are not matched");
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Registered successfully");
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            confirmpassword = textBox3.Text;
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            username = textBox1.Text;
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
